328d6f748dd2f88e6295ca8a1c73903cb446a462
JonDev22 <jonnydeveloper22@gmail.com>
2025-08-12 22:51:26 +0200

    Add Home View

--- src/tabs/home/HomeStack.tsx
+++ src/tabs/home/HomeStack.tsx
@@ -1,11 +1,13 @@
-import { Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './stacks/home/Home';
+
+const Stack = createNativeStackNavigator();
 
 function HomeStack() {
     return (
-        <SafeAreaView>
-            <Text>Hi</Text>
-        </SafeAreaView>
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
     );
 }
 
---
+++ src/tabs/home/stacks/home/Home.tsx
@@ -0,0 +1,11 @@
+import { ScrollView, Text } from 'react-native';
+
+function Home() {
+    return (
+        <ScrollView>
+            <Text>Hi</Text>
+        </ScrollView>
+    );
+}
+
+export default Home;
