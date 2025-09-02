9a641d6421fdc9114a5ab053adbce8a543b0e56d
JonDev22 <jonnydeveloper22@gmail.com>
2025-09-02 15:37:59 +0200

    Update appUrls.ts

--- src/utils/appUrls.ts
+++ src/utils/appUrls.ts
@@ -3,7 +3,7 @@ const enum appUrls {
     APPLE_PODCAST = 'https://podcasts.apple.com/de/podcast/cologne-international-baptist-church/id1742416920',
     SPOTIFY_PODCAST = 'https://open.spotify.com/show/1DR5382pZ18BZbqJYalIpg',
     APPLE_MUSIC = 'https://music.apple.com/de/playlist/ibc-cologne-music-playlist/pl.u-xlyNqMdCJxRDKNP?l=en',
-    SPOTIFY_MUSIC = 'https://open.spotify.com/artist/your-artist-id',
+    SPOTIFY_MUSIC = 'https://open.spotify.com/playlist/4P0aQkgeBD61wXsUVy0Hja?si=13d5ec43102b40a5&nd=1&utm_medium=organic&_branch_referrer=H4sIAAAAAAAAA7WNwWrDMBBEv0Y%2B2rLk5BAwpSGEXgItpSG3sJZXjuqNJFYywZd%2Be%2B1AP6Ewh2GGx7vlHNOuqlIM2dm5hBhLcn6sXiKHfjK5DRF9IVRjJ6LrxNTeVkToV6GOS9a7%2FKNNuC9TJJjJpbzU5l3Cxzjg%2FrCtH5f0dZ7l2zcIfUxO6EOt%2Bw2aRtdSdY2EzVMDRB2Y8Z9VQm19v%2B6FRcgTYxt4AO9M8cNokdn54dpxeCTk9hQ6RyiU%2FAQL7H4B1LWXezQBAAA%3D&product=open&%24full_url=https%3A%2F%2Fopen.spotify.com%2Fplaylist%2F4P0aQkgeBD61wXsUVy0Hja%3Fsi%3D13d5ec43102b40a5&feature=organic&_branch_match_id=1420495462207375857',
     INSTAGRAM = 'https://www.instagram.com/ibccologne/',
     YOUTUBE = 'https://m.youtube.com/@ibccologneonline4271',
     FACEBOOK = 'https://www.facebook.com/ibc.cologne/',
